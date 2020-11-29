d2019b0c07252905f04a4765703614baf21350c3
Dmitry Tyurnikov <DimsFromDergachy@gmail.com>
2020-11-30 00:38:03 +0400

    Add Go => Work with time

---
+++ Stepic.org/Go/3.7 Time/3.7.3.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	text, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	text = strings.TrimSpace(text)
+	t, _ := time.Parse(time.RFC3339, text)
+	fmt.Println(t.Format(time.RubyDate))
+}
