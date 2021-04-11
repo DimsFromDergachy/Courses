bcc0c800d6598f277484691932468f1cb6a507a6
Dmitry Tyurnikov <DimsFromDergachy@gmail.com>
2021-04-11 22:02:28 +0400

    Haskell => Applicative Functors => Pointed

---
+++ Stepic.org/Haskell/Applicative Functors/1.1b.hs
@@ -0,0 +1,24 @@
+-- Pointed - aka singleton, return, unit, point
+
+import Prelude hiding (pure)
+
+class Functor f = Pointed f where
+  pure :: a - f a
+
+instance Pointed Maybe where
+  pure x = Just x
+
+instance Pointed [] where
+  pure x = [x]
+
+instance Pointed (Either e) where
+  pure x = Right x
+
+instance Pointed ((-) e) where
+  pure x = const x
+
+instance Monoid s = Pointed ((,) s) where
+  pure x = (mempty , x)
+
+-- Law:
+-- fmap g (pure x) === pure (g x)
