d9170e9b894f9f14bc77fb4db7c00b5b9e30fd4b
Dmitry Tyurnikov <DimsFromDergachy@gmail.com>
2021-04-13 22:31:52 +0400

    Haskell => List vs ZipList

---
+++ Stepic.org/Haskell/Applicative Functors/1.2a.hs
@@ -0,0 +1,22 @@
+
+-- List as Applicative
+newtype List a = List [a]
+    deriving Show
+
+instance Functor List where
+    fmap f (List xs) = List $ map f xs
+
+instance Applicative List where
+    pure x = List [x]
+    List fs <*> List xs = List [f x | f <- fs, x <- xs]
+
+-- ZipList as Applicative
+newtype ZipList a = ZipList [a]
+    deriving Show
+
+instance Functor ZipList where
+    fmap f (ZipList xs) = ZipList $ map f xs
+
+instance Applicative ZipList where
+    pure x = ZipList [x]
+    ZipList fs <*> ZipList xs = ZipList $ zipWith ($) fs xs
