40f33666912b6739a718435cc2c75e71bb5cb62e
Dmitry Tyurnikov <DimsFromDergachy@gmail.com>
2021-05-22 17:20:59 +0400

    Haskell => Maybe and Pair as Traversable

--- Stepic.org/Haskell/2. Effects/2.2b.hs
+++ Stepic.org/Haskell/2. Effects/2.2b.hs
@@ -1,4 +1,4 @@
-import Prelude hiding (sequenceA, traverse)
+import Prelude hiding (Traversable, sequenceA, traverse)
 
 class (Functor t, Foldable t) => Traversable t where
     sequenceA :: Applicative f => t (f a) -> f (t a)
@@ -7,3 +7,13 @@ class (Functor t, Foldable t) => Traversable t where
     sequenceA  = traverse id
     traverse f = sequenceA . fmap f
 
+instance Traversable Maybe where
+    sequenceA Nothing  = pure Nothing
+    sequenceA (Just x) = Just <$> x
+
+    traverse _ Nothing  = pure Nothing
+    traverse f (Just x) = Just <$> f x
+
+instance Traversable ((,) s) where
+    sequenceA (s, fa) = (,) s <$> fa
+    traverse f (s, a) = (,) s <$> f a
