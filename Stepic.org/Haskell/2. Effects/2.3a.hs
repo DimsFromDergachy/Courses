f00c47ad5eca7b41722b3b71e2a7af610cba657f
Dmitry Tyurnikov <DimsFromDergachy@gmail.com>
2021-05-24 21:11:27 +0400

    Haskell => Laws for Traversable: Identity & Composition

---
+++ Stepic.org/Haskell/2. Effects/2.3a.hs
@@ -0,0 +1,29 @@
+
+class (Functor t, Foldable t) => Traversable t where
+    sequenceA :: Applicative f => t (f a) -> f (t a)
+    traverse  :: Applicative f => (a -> f b) -> t a -> f (t b)
+
+--  Laws:
+--  1. Identity
+--      traverse Identity === Identity
+--               Identity ::   a -> Identity    a
+--      traverse Identity :: t a -> Identity (t a)
+
+--  2. Composition
+--      traverse (Compose . fmap g2 . g1) === Compose . fmap (traverse g2) . traverse g1
+--
+--      fmap :: (a -> b) -> f a -> f b
+--      traverse :: (a -> f b) -> t a -> f (t b)
+--      Compose :: f (g a) -> Compose f g a
+--      g1 :: a -> f1 b
+--      g2 :: b -> f2 c
+--      
+--      fmap g2 . g1 :: a -> f1 (f2 c)
+--      Compose . fmap g2 . g1 :: a -> Compose f1 f2 c
+--      traverse (Compose . fmap g2 . g1) :: t a -> Compose f1 f2 (t c)
+--      
+--      traverse g1 :: t a -> f1 (t b)
+--      traverse g2 :: t b -> f2 (t c)
+--      fmap (traverse g2) :: f1 (t b) -> f1 (f2 (t c))
+--      fmap (traverse g2) . (traverse g1) :: t a -> f1 (f2 (t c))
+--      Compose . fmap (traverse g2) . (traverse g1) :: t a -> Compose f1 f2 (t c)
