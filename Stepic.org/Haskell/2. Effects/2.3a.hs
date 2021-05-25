f2ed8c8f0ad1f5a81fac7a5d4d29774576d14f33
Dmitry Tyurnikov <DimsFromDergachy@gmail.com>
2021-05-25 15:26:27 +0400

    Haskell => Laws for Traversable: Version for 'sequenceA'

--- Stepic.org/Haskell/2. Effects/2.3a.hs
+++ Stepic.org/Haskell/2. Effects/2.3a.hs
@@ -36,3 +36,43 @@ class (Functor t, Foldable t) => Traversable t where
 --      t :: (Applicative f, Applicative g) => f a -> g a
 --      t (pure x) === x
 --      t (x <*> y) === t x <*> t y
+
+--  Laws: (for sequenceA)
+--  1. Identity
+--      sequenceA . fmap Identity === Identity
+--
+--      Identity :: a -> Identity a
+--      fmap :: (a -> b) -> f a -> f b
+--      fmap Identity :: f a -> f (Identity a)
+--      sequenceA :: t (f a) -> f (t a)
+--      sequenceA . fmap Identity :: t a -> Identity (t a)
+
+--  2. Composition
+--      sequenceA . fmap Compose === Compose . fmap sequenceA . sequenceA
+--
+--      Compose :: f1 (f2 a) -> Compose f1 f2 a
+--      fmap :: (a -> b) -> f a -> f b
+--      fmap Compose :: h (f1 (f2 a)) -> h (Compose f1 f2 a)
+--      sequenceA :: t (f a) -> f (t a)
+--      sequenceA . fmap Compose :: t (f1 (f2 a)) -> Compose f1 f2 (t a)
+--
+--      sequenceA :: t (f1 a) -> f1 (t a)
+--      fmap :: (a -> b) -> f a -> f b
+--      sequenceA :: t (f2 a) -> f2 (t a)
+--      fmap sequenceA :: f1 (t (f2 a)) -> f1 (f2 (t a))
+--      fmap sequenceA . sequenceA :: t (f1 (f2 a)) -> f1 (f2 (t a))
+--      Compose :: f1 (f2 a) -> Compose f1 f2 a
+--      Compose . fmap sequenceA . sequenceA :: t (f1 (f2 a)) -> Compose f1 f2 (t a)
+
+--  3. Naturality
+--      t . sequenceA === sequenceA . fmap t
+--
+--      sequenceA :: (Traversable t, Applicative f) => t (f a) -> f (t a)
+--      t :: (Applicative f, Applicative g) => f a -> g a
+--      t . sequenceA :: (Traversable t, Applicative f, Applicative g) => t (f a) -> g (t a)
+--
+--      t :: (Applicative f, Applicative g) => f a -> g a
+--      fmap :: Functor f => (a -> b) -> f a -> f b
+--      fmap t :: (Applicative f, Applicative g, Functor h) => h (f a) -> h (g a)
+--      sequenceA :: (Traversable t, Applicative g) => t (g a) -> g (t a)
+--      sequenceA . fmap t :: (Traversable t, Applicative f, Applicative g) => t (f a) -> g (t a)
