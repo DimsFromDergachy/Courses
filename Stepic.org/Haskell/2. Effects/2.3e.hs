33ee6026c348d2fa9de3089b84aaf94d65df942a
Dmitry Tyurnikov <DimsFromDergachy@gmail.com>
2021-05-27 15:23:07 +0400

    Haskell => Implement 'fmap' through 'traverse'

---
+++ Stepic.org/Haskell/2. Effects/2.3e.hs
@@ -0,0 +1,18 @@
+
+import Data.Functor.Identity
+
+data Result a = Error | OK a
+    deriving (Eq, Show)
+
+instance Traversable Result where
+    sequenceA Error   = pure Error
+    sequenceA (OK fx) = OK <$> fx
+
+    traverse _ Error  = pure Error
+    traverse f (OK x) = OK <$> f x
+
+fmapDefault :: Traversable t => (a -> b) -> t a -> t b
+fmapDefault f = runIdentity . traverse (Identity . f)
+
+instance Functor Result where
+    fmap = fmapDefault
